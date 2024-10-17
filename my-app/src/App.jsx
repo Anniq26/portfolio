bb871446acc2d9bf18884acd755345d570560ca8
Giorgi Kitoshvili <aniqitoshvili2012@gmail.com>
2024-10-17 23:57:32 +0400

    n

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import {RouterProvider, createBrowserRouter} from 'react-router-dom';
-import router from "./router";
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import router from './router';
+import { Helmet } from 'react-helmet';  // Import Helmet
 
 const App = () => {
   return (
-    <div>    
-      <RouterProvider router = {createBrowserRouter(router)} />
+    <div>
+      <Helmet>
+        <title>Ani Kitoshvili</title>
+        <link rel="icon" href="/logoV2.png" />
+      </Helmet>
+      
+      <RouterProvider router={createBrowserRouter(router)} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
